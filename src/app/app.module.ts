6f5f093d51d282a95861315aaba3bbfe3658c66e
Hillary Doan <doanhillary@gmail.com>
2019-07-24 21:23:43 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MovieCallService } from './movie-call.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { WatchListPageComponent } from './watch-list-page/watch-list-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchCriteriaComponent,
     MovieListComponent,
-    WatchListPageComponent
+    WatchListPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [MovieCallService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
